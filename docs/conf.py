f43955def77ba3283e7cb1dacb4985fd77ce1e6e
Pascal Pepe <24894031+pascalpepe@users.noreply.github.com>
2023-01-07 16:20:59 +0100

    Set up GitHub Actions (#5)

--- docs/conf.py
+++ docs/conf.py
@@ -60,3 +60,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- Options for the linkcheck builder ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-linkcheck-builder
+linkcheck_ignore = [r"http://localhost:\d+/"]
